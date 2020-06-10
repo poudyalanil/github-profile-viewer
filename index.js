8d778fb86f3051cedf017c334d7159898219e2cd
Anil Poudyal <anilrunjakote@gmail.com>
2020-06-10 13:48:57 +0545

    Pagination

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ window.addEventListener('load', () => {
     let fullName = document.querySelector("#fullName");
     let detail = document.querySelector("#detail")
     let badge = document.querySelector(".badge")
+    let repoEndPoint;
+
+    let tableItems = document.querySelector(".tableItems");
+
+    // let repoTitle = document.querySelector("#repoTitle");
+    // let dateCreated = document.querySelector("#dateCreated");
+    // let language = document.querySelector("#language");
+    // let repoUrl = document.querySelector("#repoUrl");
+
+
+
+
 
 
     let twitter = document.querySelector(".twitter");
 
     submitBtn.addEventListener("click", () => {
-        console.log("clicked");
+
         getInfo(inputText.value);
         profileSection.style.display = "block";
 
@@ -27,20 +39,26 @@ window.addEventListener('load', () => {
         let endpoint = await fetch(`https://api.github.com/users/${username}`);
         let info = await endpoint.json();
         if (info.message != "Not Found") {
-
             // inputSection.style.display = "none";
-            console.log(info)
+            // console.log(info)
             profileImage.src = info.avatar_url;
             userId.textContent = info.login;
             fullName.textContent = info.name;
             detail.textContent = info.bio;
+
+
             twitterUserName = "https://twitter.com/" + info.twitter_username;
             if (twitterUserName.length > 10) {
                 twitter.href = twitterUserName;
             } else {
-                twitter.display = "none";
+                twitter.display = "none"; q
             }
             badge.textContent = info.public_repos;
+
+
+            getRepoInfo(info.repos_url);
+
+
 
         } else {
             alert("User Name Incorrect")
@@ -48,6 +66,33 @@ window.addEventListener('load', () => {
 
 
     }
+    async function getRepoInfo(repo) {
+        fetch(repo).then(data => data.json()
+            .then(data => {
+                tableItems.innerHTML = ""
+                console.log(data)
+                for (var i = 0; i < data.length; i++) {
+                    tableItems.innerHTML +=
+                        '<tr>' +
+                        '<td>' + i + 1 + '</td>' +
+                        '<td>' + data[i].name + '</td>' +
+                        '<td>' + data[i].created_at + '</td>' +
+                        '<td>' + data[i].language + '</td>' +
+                        '<td> <a target="_blank" href = "' + data[i].html_url + '">Click Here</a></td>'
+
+                        + '</tr>'
+
+
+
+
+                        ;
+                }
+
+            })
+
+        )
+
+    };
 
 
 });
